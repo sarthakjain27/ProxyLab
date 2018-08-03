014964a804f81c36e76e90967a1fb78f511f20f3
sarthakjain27 <39550950+sarthakjain27@users.noreply.github.com>
2018-08-04 01:04:11 +0530

    need to start from check()

--- cache.c
+++ cache.c
@@ -14,3 +14,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "cache.h"
+
+cnode *head,*tail;
+int rqst_in_cache;
+volatile size_t present_cache_size;
+volatile int readcnt;
+sem_t mutex,w;
+
+/*
+* Initialize the cache with default values
+*
+*/ 
+void cache_initial()
+{
+	head=NULL;
+	tail=NULL;
+	rqst_in_cache=0;
+	present_cache_size=0;
+	readcnt=0;
+	Sem_init(&mutex,0,1); // to control number readcnt update when GET request comes
+	Sem_init(&w,0,1); // to control access to cache when modification/reading is happening
+}
